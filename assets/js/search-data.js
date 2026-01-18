6dc8c248dfea563bbf322ec390dec006bedd7a02
Cliffia123 <Cliffia7@gmail.com>
2026-01-18 08:33:35 +0000

    Deploying to gh-pages from @ Cliffia123/Cliffia123.github.io@c211061dcbec1806084d24031dca495eb89b4104 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-one-technical-report-on-npu-kernel-generation-is-released-in-arxiv",
+          title: 'One Technical Report on NPU Kernel generation is released in arxiv.',
+          description: "",
+          section: "News",},{id: "news-one-paper-on-generalized-category-discovery-is-accepted-by-the-journal-iccv-2025-one-paper-on-prompt-tunning-is-accepted-by-the-journal-iccv-2025",
+          title: 'One paper on Generalized Category Discovery is accepted by the journal ICCV 2025...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
